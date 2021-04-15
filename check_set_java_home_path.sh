481c1a71f20dd06f82cf77fbc81f34034d23c402
Gus <akaisuzuki@gmail.com>
2021-04-15 13:17:37 -0300

    Update check_set_java_home_path.sh

--- check_set_java_home_path.sh
+++ check_set_java_home_path.sh
@@ -8,7 +8,7 @@ echo "......................................................."
 echo "PRESS ENTER TO VIEW THE TEMPLATE NEEDED TO SETUP/CHECK"
 echo "THE PATH IN THE ~/.bashrc FILE..."
 read
-cat /home/$USER/jdk_openjdk_java_javac_installation/java_home.txt
+cat /home/$USER/jdk_openjdk_java_javac_installation_v2/java_home.txt
 echo "......................................................."
 echo "COPY AND PASTE THE TEMPLATE AT THE END OF THE  ~/.bashrc"
 echo "FILE, IF IT IS STILL NOT SET."
@@ -20,10 +20,10 @@ echo "......................................................."
 echo "FOR CHANGES TO TAKE EFFECT, CLOSE THIS TERMINAL/SESSION"
 echo "AND RE-OPEN A NEW ONE."
 echo "ENTER THE FOLLOWING LINE: "
-cat  /home/$USER/jdk_openjdk_java_javac_installation/java_home_path.txt
+cat  /home/$USER/jdk_openjdk_java_javac_installation_v2/java_home_path.txt
 echo "......................................................."
 echo "YOU SHOULD GET THE OUTPUT OF THE: "
-cat  /home/$USER/jdk_openjdk_java_javac_installation/java_home_path.txt
+cat  /home/$USER/jdk_openjdk_java_javac_installation_v2/java_home_path.txt
 echo "......................................................."
 echo "PROGRAM TERMINATED. PRESS ENTER TO TERMINATE SESSION NOW..."
 read
